40fa8657ed93cf8df5586ed65dd69b9ceba6e6fc
Rebecca Schiele <rschiele9807@sdsu.edu>
2024-04-12 20:24:35 -0700

    did a test run on a high performance computer in the 24 hour library area. After 30 minutes, couldn't finish updating U. That was after shrinking U and V size. Will get home safely and try to test the 30 minute progress on my laptop for comparison. Will also try an overnight full run to see if that has a better result than my attempt at overnight debugging last night.

--- out/build/x64-debug/external/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Owner/source/repos/S24CS696hw/S24CS696PS2P2/external
-# Build directory: C:/Users/Owner/source/repos/S24CS696hw/S24CS696PS2P2/out/build/x64-debug/external
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
